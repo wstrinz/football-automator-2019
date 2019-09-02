1791e7484b1915446dd2fdd43e529131eb14a442
Will Strinz <wstrinz@gmail.com>
2019-09-02 15:12:05 -0500

    working lineup setter

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,132 @@ import { callAlert } from "./other";
 
 console.log("running script");
 
-let playerRows = (): HTMLElement[] => {
-  let playersTable = document.querySelector(
-    "#espn-analytics > div > div.jsx-3010562182.shell-container > div.page-container.cf > div.layout.is-full > div > div > div > div.jsx-639395163.rosterContainer.relative > div > div > section > table > tbody > tr > td > div > div > div.Table2__shadow-scroller > table > tbody > tr > td > div > table"
-  );
+// Extract to step/action utils
 
-  return Array.prototype.slice.call(
-    playersTable.querySelectorAll("tbody > tr")
+let sleep = (time: number) => {
+  return new Promise(resolve => setTimeout(resolve, time));
+};
+
+let waitForCondition = (callback: { () }) => {
+  return new Promise(async accept => {
+    do {
+      await sleep(500);
+    } while (!callback());
+    accept();
+  });
+};
+
+let waitForCss = async (selector: string) => {
+  await waitForCondition(() => document.querySelector(selector));
+};
+
+let maxBy = <T>(array: T[], callback: { (x: any): number }): T => {
+  let max = Math.max(...array.map(o => callback(o)).filter(v => !isNaN(v)));
+
+  return array.find(o => callback(o) == max);
+};
+
+// extract to player file
+
+interface Player {
+  currentPosition: String;
+  projected?: number;
+}
+
+interface PlayerWithRow {
+  row: HTMLElement;
+  info: Player;
+}
+
+let scrapePlayerRow = (row: HTMLElement): Player => {
+  const player = {} as Player;
+
+  player.currentPosition = row.querySelector("td:nth-child(1)").textContent;
+
+  let projText = row.querySelector("td:nth-child(6)").textContent;
+  if (projText != "--") {
+    player.projected = parseFloat(projText);
+  }
+
+  return player;
+};
+
+let playersTable = () => {
+  return document.querySelector(
+    "#espn-analytics > div > div.jsx-3010562182.shell-container > div.page-container.cf > div.layout.is-full > div > div > div > div.jsx-639395163.rosterContainer.relative > div > div > section > table > tbody > tr > td > div > div > div.Table2__shadow-scroller > table > tbody > tr > td > div > table"
   );
 };
 
-let availableSlots = (): HTMLElement[] => {
-  console.log(playerRows());
-  let openHereSlots = playerRows().filter(row =>
+let playerRows = (): HTMLElement[] => {
+  return Array.prototype.slice.call(
+    playersTable().querySelectorAll("tbody > tr")
+  );
+};
+
+let activePlayers = (): HTMLElement[] => {
+  return playerRows().filter(
+    r => scrapePlayerRow(r).currentPosition != "Bench"
+  );
+};
+
+let benchPlayers = (): HTMLElement[] => {
+  return playerRows().filter(
+    r => scrapePlayerRow(r).currentPosition == "Bench"
+  );
+};
+
+let availableBenchSlots = (): HTMLElement[] => {
+  let openHereSlots = benchPlayers().filter(row =>
     row.querySelector('button[title="HERE"]')
   );
-  console.log(openHereSlots);
   return openHereSlots;
 };
 
-let setLineup = () => {
-  let dak = playerRows()[0];
-  let dakButton = dak.querySelector(
+// Extract to optimizers
+
+let trySwapPlayer = async (playerRow: HTMLElement) => {
+  let actionButton = playerRow.querySelector(
     "td > div[title='Action'] button"
   ) as HTMLElement;
-  dakButton.click();
+  actionButton.click();
 
-  setTimeout(() => {
-    let slotButton = availableSlots()[0].querySelector(
+  await waitForCss('button[title="HERE"]');
+
+  let playerInfos = availableBenchSlots().map(row => {
+    const pdata = {} as PlayerWithRow;
+    pdata.row = row;
+    pdata.info = scrapePlayerRow(row);
+
+    return pdata;
+  });
+
+  let bestChoice = maxBy(playerInfos, p => p.info.projected);
+
+  if (
+    bestChoice &&
+    bestChoice.info.projected > scrapePlayerRow(playerRow).projected
+  ) {
+    let slotButton = bestChoice.row.querySelector(
       'button[title="HERE"]'
     ) as HTMLElement;
     slotButton.click();
-  }, 1000);
+  } else {
+    let slotButton = playerRow.querySelector(
+      'button[title="MOVE"]'
+    ) as HTMLElement;
+    slotButton.click();
+  }
+
+  await waitForCondition(() => !document.querySelector('button[title="HERE"]'));
+};
+
+let setLineup = async () => {
+  // For each player, see if there's any beneficial swaps to bench
+  // maybe keep track of swaps and run until no change or timeout
+
+  for (let pr of activePlayers()) {
+    await trySwapPlayer(pr);
+  }
 };
 
 let enhancePage = () => {
@@ -47,7 +141,4 @@ let enhancePage = () => {
   document.querySelector("div.myTeamButtons").appendChild(button);
 };
 
-console.log("TODO: more cleverly wait for page load");
-setTimeout(() => {
-  enhancePage();
-}, 8000);
+waitForCss("div.myTeamButtons").then(() => enhancePage());
